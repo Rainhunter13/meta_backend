b33a87182dff9de4dacfcf2c72f2f9a495dea25e
Rakhman Asmatullayev <rakhmandva@gmail.com>
2021-02-07 16:54:43 +0600

    Defined Therapist and Method models

--- therapists_profiles/admin.py
+++ therapists_profiles/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Therapist, Method
 
 # Register your models here.
+admin.site.register(Therapist)
+admin.site.register(Method)
--- therapists_profiles/models.py
+++ therapists_profiles/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
+
 # Create your models here.
+class Therapist(models.Model):
+    name = models.CharField(default=None, null=True, max_length=100)
+    photo_url = models.CharField(default=None, null=True, max_length=100)
+
+
+class Method(models.Model):
+    name = models.CharField(max_length=100)
+    therapist = models.ForeignKey(Therapist, related_name='methods', on_delete=models.CASCADE)
