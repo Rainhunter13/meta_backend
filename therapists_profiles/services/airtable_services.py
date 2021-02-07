9898c100def3c36a99e430673bfc7278a858129d
Rakhman Asmatullayev <rakhmandva@gmail.com>
2021-02-08 01:55:23 +0600

    Implemented Functionality for synchronizing Airtable with PostgreSQL

--- therapists_profiles/services/airtable_services.py
+++ therapists_profiles/services/airtable_services.py
@@ -1,12 +1,79 @@
+"""Services for working with data from airtable"""
+
 from airtable import Airtable
 from configurations.config import base_key, api_key
+from therapists_profiles.models import Therapist, Method
 
-airtable = Airtable(base_key=base_key, table_name="Psychotherapists", api_key=api_key)
 
-therapists = []
-for therapist in airtable.get_all():
-    therapists.append({
-        'name': therapist['fields']['Имя'],
-        'photo_url': therapist['fields']['Фотография'][0]['url'],
-        'methods': therapist['fields']['Методы']
-    })
+def authenticate_airtable(table_name):
+    airtable = Airtable(base_key=base_key, table_name=table_name, api_key=api_key)
+    return airtable
+
+
+def get_therapists_from_airtable(table_name):
+    """Returns a list of therapists in the airtable"""
+    airtable = authenticate_airtable(table_name)
+
+    therapists_dicts = []
+    for airtable_therapist in airtable.get_all():
+        therapists_dicts.append({
+            'airtable_id': airtable_therapist['id'],
+            'name': airtable_therapist['fields']['Имя'],
+            'photo_url': airtable_therapist['fields']['Фотография'][0]['url'],
+            'methods': airtable_therapist['fields']['Методы']
+        })
+
+    return therapists_dicts
+
+
+def sync_airtable_with_postgres(table_name):
+    """Make changes in PostgreSQL database so that therapist list is same as in Airtable"""
+    therapists_dicts = get_therapists_from_airtable(table_name)
+    airtable_ids = set()
+
+    for therapist_dict in therapists_dicts:
+        airtable_ids.add(therapist_dict['airtable_id'])
+        if not Therapist.objects.filter(airtable_id=therapist_dict['airtable_id']):
+            add_therapist_to_postgres(therapist_dict)
+        else:
+            update_therapist_in_postgres(therapist_dict)
+
+    for therapist in Therapist.objects.all():
+        if therapist.airtable_id not in airtable_ids:
+            therapist.delete()
+
+
+def add_therapist_to_postgres(therapist_dict):
+    """Adds a new therapist to PostgreSQL database"""
+    new_therapist = Therapist(
+        airtable_id=therapist_dict['airtable_id'],
+        name=therapist_dict['name'],
+        photo_url=therapist_dict['photo_url']
+    )
+    new_therapist.save()
+
+    for method_name in therapist_dict['methods']:
+        if not Method.objects.filter(name=method_name):
+            new_method = Method(name=method_name)
+            new_method.save()
+            new_method.therapist.add(new_therapist)
+        else:
+            method = Method.objects.get(name=method_name)
+            method.therapist.add(new_therapist)
+
+
+def update_therapist_in_postgres(therapist_dict):
+    """Updates therapist fields in PostgreSQL database"""
+    old_therapist = Therapist.objects.get(airtable_id=therapist_dict['airtable_id'])
+    old_therapist.name = therapist_dict['name']
+    old_therapist.photo_url = therapist_dict['photo_url']
+
+    for method_name in therapist_dict['methods']:
+        if not Method.objects.filter(name=method_name):
+            new_method = Method(name=method_name)
+            new_method.save()
+            new_method.therapist.add(old_therapist)
+        else:
+            method = Method.objects.get(name=method_name)
+            if old_therapist not in method.therapist.all():
+                method.therapist.add(old_therapist)
--- therapists_profiles/tests.py
+++ therapists_profiles/tests.py
@@ -1,3 +1,31 @@
-from django.test import TestCase
+import os
+import django
 
-# Create your tests here.
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "meta_fullstack.settings")
+django.setup()
+
+
+def clear_test_table(airtable):
+    for record in airtable.get_all():
+        airtable.delete(record['id'])
+
+
+def fill_test_table(airtable):
+    airtable.insert({
+        'Имя': 'Test_Name',
+        'Методы': ['test_method_1', 'test_method_2'],
+    })
+
+
+def test_get_therapists_from_airtable():
+    from therapists_profiles.services.airtable_services import get_therapists_from_airtable
+    airtable_therapists = get_therapists_from_airtable('Test_Table')
+    assert airtable_therapists[0]['name'] == 'Test_Name'
+
+
+def test_sync_airtable_with_postgres():
+    from therapists_profiles.services.airtable_services import sync_airtable_with_postgres
+    from therapists_profiles.models import Therapist
+    sync_airtable_with_postgres('Test_Table')
+    assert Therapist.objects.first().name == 'Test_Name'
+    sync_airtable_with_postgres('Psychotherapists')
