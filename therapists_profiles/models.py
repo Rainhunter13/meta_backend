8ed63b305c26592975cd1d63f1e7876f6ecbe57f
Rakhman Asmatullayev <rakhmandva@gmail.com>
2021-02-07 18:38:28 +0600

    Set ManyToMany relation between Therapist and Method models

--- therapists_profiles/models.py
+++ therapists_profiles/models.py
@@ -1,13 +1,17 @@
+"""Define ORM models here"""
+
 from django.db import models
 
 
 # Create your models here.
 class Therapist(models.Model):
+    """Model for keeping therapists information"""
     airtable_id = models.CharField(max_length=100, default=None)
     name = models.CharField(default=None, null=True, max_length=100)
     photo_url = models.CharField(default=None, null=True, max_length=100)
 
 
 class Method(models.Model):
+    """Model for keeping methods related to therapists"""
     name = models.CharField(max_length=100, default=None)
-    therapist = models.ForeignKey(Therapist, related_name='methods', on_delete=models.CASCADE)
+    therapist = models.ManyToManyField(Therapist)
