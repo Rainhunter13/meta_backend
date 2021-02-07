4be3881a9573237ad331caa43db8c608e57f2ef6
Rakhman Asmatullayev <rakhmandva@gmail.com>
2021-02-07 18:30:46 +0600

    Added airtable_id field to Therapist model

--- therapists_profiles/models.py
+++ therapists_profiles/models.py
@@ -3,10 +3,11 @@ from django.db import models
 
 # Create your models here.
 class Therapist(models.Model):
+    airtable_id = models.CharField(max_length=100, default=None)
     name = models.CharField(default=None, null=True, max_length=100)
     photo_url = models.CharField(default=None, null=True, max_length=100)
 
 
 class Method(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=100, default=None)
     therapist = models.ForeignKey(Therapist, related_name='methods', on_delete=models.CASCADE)
