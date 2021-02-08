cdb1114f9218c6c81ec5c68715ec423e6e23aa4b
Rakhman Asmatullayev <rakhmandva@gmail.com>
2021-02-09 01:57:24 +0600

    Unnecessary if removed

--- therapists_profiles/views.py
+++ therapists_profiles/views.py
@@ -6,17 +6,16 @@ from therapists_profiles.models import Therapist
 
 def profiles(request):
     """Returns a list of therapist in database as a list of dictionaries"""
-    if request.method == 'GET':
-        therapists = []
-        for therapist in Therapist.objects.all():
-            methods = []
-            for method in therapist.methods.all():
-                methods.append(method.name)
-            therapists.append({
-                'id': therapist.id,
-                'name': therapist.name,
-                'photo_url': therapist.photo_url,
-                'methods': methods,
-                'airtable_id': therapist.airtable_id,
-            })
-        return HttpResponse(therapists)
+    therapists = []
+    for therapist in Therapist.objects.all():
+        methods = []
+        for method in therapist.methods.all():
+            methods.append(method.name)
+        therapists.append({
+            'id': therapist.id,
+            'name': therapist.name,
+            'photo_url': therapist.photo_url,
+            'methods': methods,
+            'airtable_id': therapist.airtable_id,
+        })
+    return HttpResponse(therapists)
