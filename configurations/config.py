80eefc453c3a6735a1555f574db870eb917c16e4
Rakhman Asmatullayev <rakhmandva@gmail.com>
2021-02-08 15:59:36 +0600

    Deleted config.py

--- configurations/config.py
+++
@@ -1,8 +0,0 @@
-secret_key = 'here_is_your_django_secret_key'
-
-db_name = 'here_is_your_database_name'
-db_user = 'here_is_your_postgres_username'
-db_password = 'here_is_your_database_password'
-
-base_key = 'here_is_your_airtable_base_key'
-api_key = 'here_is_your_airtable_api_key'
