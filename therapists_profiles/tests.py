88d55947f13578c3bedd45ceb2cbb5b66b67afbf
Rakhman Asmatullayev <rakhmandva@gmail.com>
2021-02-08 14:38:20 +0600

    Tests restructured: pytest -> Django Tests, pylint-django added

--- therapists_profiles/tests.py
+++ therapists_profiles/tests.py
@@ -1,31 +1,40 @@
+"""therapists_profiles app tests"""
+
 import os
 import django
+from django.test import TestCase
+
+from therapists_profiles.services.airtable_services import get_therapists_from_airtable
+from therapists_profiles.services.airtable_services import sync_airtable_with_postgres
+from therapists_profiles.models import Therapist
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "meta_fullstack.settings")
 django.setup()
 
 
-def clear_test_table(airtable):
-    for record in airtable.get_all():
-        airtable.delete(record['id'])
-
-
 def fill_test_table(airtable):
+    """fills test airtable with test values"""
     airtable.insert({
         'Имя': 'Test_Name',
         'Методы': ['test_method_1', 'test_method_2'],
     })
 
 
-def test_get_therapists_from_airtable():
-    from therapists_profiles.services.airtable_services import get_therapists_from_airtable
-    airtable_therapists = get_therapists_from_airtable('Test_Table')
-    assert airtable_therapists[0]['name'] == 'Test_Name'
+def clear_test_table(airtable):
+    """clears test airtable"""
+    for record in airtable.get_all():
+        airtable.delete(record['id'])
 
 
-def test_sync_airtable_with_postgres():
-    from therapists_profiles.services.airtable_services import sync_airtable_with_postgres
-    from therapists_profiles.models import Therapist
-    sync_airtable_with_postgres('Test_Table')
-    assert Therapist.objects.first().name == 'Test_Name'
-    sync_airtable_with_postgres('Psychotherapists')
+class AirtableTestCase(TestCase):
+    """Class for testing airtable services"""
+
+    def test_get_therapists_from_airtable(self):
+        """Test for get_therapists_from_airtable()"""
+        airtable_therapists = get_therapists_from_airtable('Test_Table')
+        self.assertEqual(airtable_therapists[0]['name'], 'Test_Name')
+
+    def test_sync_airtable_with_postgres(self):
+        """Test for sync_airtable_with_postgres()"""
+        sync_airtable_with_postgres('Test_Table')
+        self.assertEqual(Therapist.objects.first().name, 'Test_Name')
