8f5eec5ef4627442071de3815c65027d95f5b2e0
Rakhman Asmatullayev <rakhmandva@gmail.com>
2021-02-09 13:24:03 +0600

    API result -> JsonResponse

--- therapists_profiles/views.py
+++ therapists_profiles/views.py
@@ -1,21 +1,22 @@
 """Url views"""
 
-from django.http import HttpResponse
+from django.http import JsonResponse
 from therapists_profiles.models import Therapist
 
 
 def profiles(request):
     """Returns a list of therapist in database as a list of dictionaries"""
-    therapists = []
+    therapists = {}
     for therapist in Therapist.objects.all():
         methods = []
         for method in therapist.methods.all():
             methods.append(method.name)
-        therapists.append({
+        therapists[therapist.id] = {
             'id': therapist.id,
             'name': therapist.name,
             'photo_url': therapist.photo_url,
             'methods': methods,
             'airtable_id': therapist.airtable_id,
-        })
-    return HttpResponse(therapists)
+        }
+
+    return JsonResponse(therapists)
