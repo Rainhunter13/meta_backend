d0344e7eb0090148772b0125ba6492eeabb4df20
Rakhman Asmatullayev <rakhmandva@gmail.com>
2021-02-09 18:02:15 +0600

    Added endpoint for a single therapist

--- therapists_profiles/urls.py
+++ therapists_profiles/urls.py
@@ -4,5 +4,6 @@ from django.urls import path
 from therapists_profiles import views
 
 urlpatterns = [
-    path('profiles/', views.profiles)
+    path('profiles/', views.profiles),
+    path('profiles/<int:id>/', views.profile)
 ]
--- therapists_profiles/views.py
+++ therapists_profiles/views.py
@@ -18,5 +18,20 @@ def profiles(request):
             'methods': methods,
             'airtable_id': therapist.airtable_id,
         }
-
     return JsonResponse(therapists)
+
+
+def profile(request, id):
+    """Returns a therapist as a dictionary"""
+    therapist = Therapist.objects.get(id=id)
+    methods = []
+    for method in therapist.methods.all():
+        methods.append(method.name)
+    therapists_dict = {
+        'id': therapist.id,
+        'name': therapist.name,
+        'photo_url': therapist.photo_url,
+        'methods': methods,
+        'airtable_id': therapist.airtable_id,
+    }
+    return JsonResponse(therapists_dict)
